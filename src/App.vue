331411f7d561be7cdfdb2f19ec6b7f3adf716702
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-11-03 21:55:17 +0300

    save

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <a href="/first-app">First App</a> |
+      <a href="/second-app">Second App</a>
     </nav>
     <router-view/>
   </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js 2222222App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HelloWorld from '../../../general/components/HelloWorld.vue'
 export default {
   name: 'HomeView',
   components: {
